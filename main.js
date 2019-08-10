5a9f57df39a8d84056f4d145ce65cab0eba48ddb
LoganTann <logon313@hotmail.fr>
2019-08-10 23:53:21 +0200

    The mod and stylesheet works git diff

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+let volume = 0;
+
 function doStuff(){
-  
+  ig.music.volume = volume;
+  $("body").append('<p id="LOL">It works !!</p>');
+  $("head").append('<link href="mods/CrossCode-NyanIt/main.css" rel="stylesheet"/>');
+  console.log("test");
 }
 
 if (cc) doStuff();
